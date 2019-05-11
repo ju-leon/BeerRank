f8ed438356b7fecd16187d583908c466ff0cc382
Leon Jungemeyer <leon.jungemeyer@me.com>
2019-05-11 12:11:29 +0200

    Added Spring Boot Backend

---
+++ Server/beerrank/src/main/java/com/beerrank/server/beerrank/BeerrankApplication.java
@@ -0,0 +1,13 @@
+package com.beerrank.server.beerrank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeerrankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeerrankApplication.class, args);
+	}
+
+}
---
+++ Server/beerrank/src/test/java/com/beerrank/server/beerrank/BeerrankApplicationTests.java
@@ -0,0 +1,16 @@
+package com.beerrank.server.beerrank;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BeerrankApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
