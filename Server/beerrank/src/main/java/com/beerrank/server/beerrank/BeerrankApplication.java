d09db4810135a2906463a7ba3b8cbd40f5b5ccb0
Leon Jungemeyer <leon.jungemeyer@me.com>
2019-05-11 14:15:39 +0200

    Added mySQL to Spring

--- Server/beerrank/src/main/java/com/beerrank/server/beerrank/BeerrankApplication.java
+++ Server/beerrank/src/main/java/com/beerrank/server/beerrank/BeerrankApplication.java
@@ -2,9 +2,17 @@ package com.beerrank.server.beerrank;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class BeerrankApplication {
+	@GetMapping("/")
+	String home() {
+		return "Server is running!";
+	}
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(BeerrankApplication.class, args);
