0ffeee62e6d04125b3fd4c0d8199e36b6370b66f
Leon Jungemeyer <leon.jungemeyer@me.com>
2019-05-11 18:25:18 +0200

    Added model

---
+++ Backend/src/main/java/com/example/demo/model/Game.java
@@ -0,0 +1,43 @@
+package com.example.demo.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "games")
+public class Game {
+
+
+    private String team1;
+    private String team2;
+
+    private int result;
+
+    private String state;
+
+    @Id
+    private String _id;
+
+    public String getTeam1() {
+        return team1;
+    }
+
+    public String getTeam2() {
+        return team2;
+    }
+
+    public int getResult() {
+        return result;
+    }
+
+    public String get_id() {
+        return _id;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+}
---
+++ Backend/src/main/java/com/example/demo/model/User.java
@@ -0,0 +1,49 @@
+package com.example.demo.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import javax.persistence.Column;
+
+@Document(collection = "users")
+public class User {
+
+    @Id
+    @Column(name = "username", updatable = false, nullable = false)
+    private String username;
+
+    private String firstName;
+    private String lastName;
+
+    @Indexed(unique = true)
+    private String email;
+
+    private String password;
+
+    private int score;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public int getScore() {
+        return score;
+    }
+}
