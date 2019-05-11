9488a47446916ec96153cf43e2c5c2f82e6e877e
Vicy <victoria.burkard@it.konicaminolta.de>
2019-05-11 13:00:37 +0200

    Added login and landingpage structure

---
+++ Client/src/components/Landingpage.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './landingpage.css';
+
+function landingpage() {
+  return (
+    <div className="Landingpage">
+      <div>
+          <div>Ready for some Beerpong?</div>
+          <button>LogIn</button>
+          <button>SignUp</button>
+      </div>
+    </div>
+  );
+}
+
+export default Landingpage;
