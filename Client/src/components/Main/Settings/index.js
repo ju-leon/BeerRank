202ad817f899e7b0eb7569640a34d52b91afa493
Vicy <victoria.burkard@it.konicaminolta.de>
2019-05-11 23:30:52 +0200

    Added back navigation to Settings

--- Client/src/components/Main/Settings/index.js
+++ Client/src/components/Main/Settings/index.js
@@ -1,8 +1,19 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+
 
 export default (props) => {
-  return (<div>Settings
-  <div>Username:{props.userName}</div>
+  return (
+    <div className="Settings">
+      <div className="Header">
+        <div className="back">
+          <Link to="/home">
+            <i className="angle left big icon"></i>
+          </Link>
+        </div>
+      </div>
+    <div>Settings</div>
+    <div>Username:{props.userName}</div>
   </div>
   )
 }
