60f62eb941033e9ddb8f3e08fa2ac385ae86f506
Vicy <victoria.burkard@it.konicaminolta.de>
2019-05-12 14:05:14 +0200

    PostGame View

--- Client/src/components/Main/Game/PostGame/index.js
+++ Client/src/components/Main/Game/PostGame/index.js
@@ -1,22 +1,37 @@
-import React from 'react'
-import {reduxForm, Field} from 'redux-form'
+import React, { useState } from 'react'
+import { reduxForm, Field } from 'redux-form'
 
 import './PostGame.sass'
 
-let PostGame = ({handleSubmit}) => {
-  const _handleSubmit = ({score}) => {
-    alert(score)
+let PostGame = ({ handleSubmit }) => {
+  const [scoreSubmitMode, setScoreSubmitMode] = useState("read");
+  const _handleSubmit = ({ score }) => {
+    console.log(score)
+    console.log({ score })
+    setScoreSubmitMode(scoreSubmitMode === "read" ? "submitted" : "read")
   }
 
   return (
     <div className="PostGame ui container">
       <form className="ui form" onSubmit={handleSubmit(_handleSubmit)}>
         <div className="field">
-          <label>Score</label>
-          <Field name="score" type="number" component="input" /> 
+          <h1>How many cups does your team have to drink?</h1>
+          <Field name="score" type="number" component="input" />
         </div>
       </form>
-    </div>
+      {
+        scoreSubmitMode === 'submitted'
+          ? (1 > 0
+            ? <div className="fix">Next time, you will rock the game!</div>
+            : <div className="fix">Yeah, Congrats!</div>)
+
+          : ''
+      }
+      <div>
+        <button className="primary">Next Game!</button>
+        <button className="secondary ghost">Home</button>
+      </div>
+    </div >
   )
 }
 
