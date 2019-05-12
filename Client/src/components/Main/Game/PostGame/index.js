2adfc58c1c3d181bc761227df307d13fb96e7dde
Alex K <x.klein.14@gmail.com>
2019-05-12 11:06:21 +0200

    post-game

--- Client/src/components/Main/Game/PostGame/index.js
+++ Client/src/components/Main/Game/PostGame/index.js
@@ -1,9 +1,27 @@
 import React from 'react'
+import {reduxForm, Field} from 'redux-form'
 
-export default () => {
+import './PostGame.sass'
+
+let PostGame = ({handleSubmit}) => {
+  const _handleSubmit = ({score}) => {
+    alert(score)
+  }
+
   return (
-    <div className="PostGame">
-      
+    <div className="PostGame ui container">
+      <form className="ui form" onSubmit={handleSubmit(_handleSubmit)}>
+        <div className="field">
+          <label>Score</label>
+          <Field name="score" type="number" component="input" /> 
+        </div>
+      </form>
     </div>
   )
 }
+
+PostGame = reduxForm({
+  form: 'gameResult'
+})(PostGame)
+
+export default PostGame
